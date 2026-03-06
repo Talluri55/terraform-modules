853b06913a66a9c8cf2b356cdf65968f5ec4c26f
Talluri55 <108296073+Talluri55@users.noreply.github.com>
2026-03-06 14:22:19 -0600

    Enable IAM Roles for Service Accounts (IRSA)

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -7,6 +7,8 @@ module "eks" {
     vpc_id          = var.vpc_id
     subnets_ids         = var.private_subnets
 
+    enable_irsa = true
+
     eks_managed_node_groups = {
         default = {
             node_instance_type = [var.node_instance_type]
