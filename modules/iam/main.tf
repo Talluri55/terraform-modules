08a6541bdbcd1af4394f6c48d2e9859c69d63436
Manoj Talluri <manojtalluri@ManojTalluri.local>
2026-03-06 10:13:18 -0600

    add IAM module

---
+++ modules/iam/main.tf
@@ -0,0 +1,10 @@
+resource "aws_iam_role" "this" {
+  name               = var.role_name
+  assume_role_policy = var.assume_role_policy
+}
+
+resource "aws_iam_role_policy_attachment" "attach" {
+  for_each = toset(var.policy_arns)
+  role     = aws_iam_role.this.name
+  policy_arn = each.value
+}
