9a3a7e1ff283a57566f7c33c9ffbdc8debc29bab
Talluri55 <108296073+Talluri55@users.noreply.github.com>
2026-03-06 11:46:51 -0600

    Update VPC outputs to use subnet values

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -3,9 +3,9 @@ output "vpc_id" {
 }
 
 output "private_subnets_id" {
-  value = module.vpc.private_subnets_id
+  value = module.vpc.private_subnets
 }
 
 output "public_subnets_id" {
-  value = module.vpc.public_subnets_id
+  value = module.vpc.public_subnets
 }
