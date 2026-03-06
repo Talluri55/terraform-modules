a7f6d5b2fe1aa2003496027df287b3e468dd81da
Talluri55 <108296073+Talluri55@users.noreply.github.com>
2026-03-06 11:58:27 -0600

    Rename outputs for private and public subnets

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -2,10 +2,10 @@ output "vpc_id" {
   value = module.vpc.vpc_id
 }
 
-output "private_subnets_id" {
+output "private_subnets" {
   value = module.vpc.private_subnets
 }
 
-output "public_subnets_id" {
+output "public_subnets" {
   value = module.vpc.public_subnets
 }
